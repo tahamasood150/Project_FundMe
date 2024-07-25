d5ae7ce661d32aabd5d32091d25d1fca1b6ec7bb
secretauditor <tahamasood150@gmail.com>
2024-07-25 20:26:15 +0500

    Some more functionalites added

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,51 +1,43 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.20;
-import "../ChainLink_Interface/AggregatorV3Interface.sol";
+import "../contracts/ChainLink_Interface/AggregatorV3Interface.sol";
 
-contract Fundme{
+contract Fundme {
 
     AggregatorV3Interface private PriceETHfetch;
     address public admin;
+    uint public minimun_funding_valueinusd = 5e18;
+    address[] public ourfunders;
+    mapping(address => uint) public ourfunders_contributedamount;
 
-    constructor(){
+    constructor() {
         // The address is sepolia test net one
         PriceETHfetch = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         admin = msg.sender;
     }
 
-     // 1e18 = 1 Ether
-    uint public minimun_funding_valueinusd = 5e18 ;
-
 
     function receive_funds() public payable {
-        require(getConversionRate(msg.value) >= minimun_funding_valueinusd ,"Minimum amount to send is : $5 ");
-
-
-
+        require(getConversionRate(msg.value) >= minimun_funding_valueinusd,"Minimum amount to send is : $5 ");
+        ourfunders.push(msg.sender);
+        ourfunders_contributedamount[msg.sender] += msg.value;
     }
 
+    function withdraw_funds_adminonly(address payable _to) public {
+        require(msg.sender == admin, "You are not the admin");
+        _to.transfer(address(this).balance);
+    }
 
-
-    function getPrice() public view returns(uint256){
-        (,int256 answer,,,) = PriceETHfetch.latestRoundData();
+    function getPrice() public view returns (uint256) {
+        (, int256 answer, , , ) = PriceETHfetch.latestRoundData();
         return uint256(answer * 1e10);
     }
 
-
-    function getConversionRate(uint ethAmount)public returns(uint) {
-        // 2000 $
-        // 2  --->> 2 * 2000 =  4000$
-        (uint answerr) = getPrice();
+    function getConversionRate(uint ethAmount) public view returns (uint) {
+        uint answerr = getPrice();
         uint ethamountin_usd = (answerr * ethAmount) / 1e18;
         return ethamountin_usd;
     }
-
-function withdraw_funds_adminonly(address payable _to) public {
-    require(msg.sender == admin, "You are not the admin");
-    _to.transfer(address(this).balance);
-}
-
-
 
 }
