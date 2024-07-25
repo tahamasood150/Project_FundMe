556ee5abc7da6bd4603d60ec405e44383e2acb06
secretauditor <tahamasood150@gmail.com>
2024-07-25 10:47:56 +0500

    Basic Functions completed

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -6,20 +6,20 @@ import "../ChainLink_Interface/AggregatorV3Interface.sol";
 contract Fundme{
 
     AggregatorV3Interface private PriceETHfetch;
+    address public admin;
 
     constructor(){
         // The address is sepolia test net one
         PriceETHfetch = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        admin = msg.sender;
     }
 
      // 1e18 = 1 Ether
-    uint public minimun_funding_valueinusd;
+    uint public minimun_funding_valueinusd = 5e18 ;
 
 
     function receive_funds() public payable {
-        (,int ethcurrent_price,,,) = PriceETHfetch.latestRoundData();
-        uint(ethcurrent_price) =  minimun_funding_valueinusd;
-        require(msg.value >= minimun_funding_valueinusd,"Minimum amount to send is : $5 ");
+        require(getConversionRate(msg.value) >= minimun_funding_valueinusd ,"Minimum amount to send is : $5 ");
 
 
 
@@ -27,9 +27,24 @@ contract Fundme{
 
 
 
-    function getPrice() public {}
-    function getConversionRate()public{}
-    function withdraw_funds_adminonly() public {}
+    function getPrice() public view returns(uint256){
+        (,int256 answer,,,) = PriceETHfetch.latestRoundData();
+        return uint256(answer * 1e10);
+    }
+
+
+    function getConversionRate(uint ethAmount)public returns(uint) {
+        // 2000 $
+        // 2  --->> 2 * 2000 =  4000$
+        (uint answerr) = getPrice();
+        uint ethamountin_usd = (answerr * ethAmount) / 1e18;
+        return ethamountin_usd;
+    }
+
+function withdraw_funds_adminonly(address payable _to) public {
+    require(msg.sender == admin, "You are not the admin");
+    _to.transfer(address(this).balance);
+}
 
 
 
